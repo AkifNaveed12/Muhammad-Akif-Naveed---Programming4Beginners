9e95e940620b0838d6d89f7174636fe1dc53c65d
Muhammad Akif Naveed <akif27031@gmail.com>
2025-10-10 15:32:34 +0500

    Create sumOf5DigitLink.c
    Task
    
    Given a five digit integer, print the sum of its digits.
    
    Input Format
    
    The input contains a single five digit number, .
    
    Constraints
    
    
    Output Format
    
    Print the sum of the digits of the five digit number.
    
    Sample Input 0
    
    10564
    Sample Output 0
    
    16

---
+++ sumOf5DigitLink.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n, sum=0;
+    scanf("%d", &n);
+    
+    for (int i =0; i<5; i++) {
+       int  j = n%10;
+       n= n/10;
+       sum = sum + j;
+    }
+    printf("%d",sum);
+    return 0;
+}
