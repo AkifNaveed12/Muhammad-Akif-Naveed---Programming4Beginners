b7c7180a4d3ff1e7907c72976dcafde4617886ee
Muhammad Akif Naveed <akif27031@gmail.com>
2025-10-16 17:57:11 +0500

    Implement maxOfFour function in C
    Task
    
    Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them.
    
    Note
    
    There is not built in max function in C. Code that will be reused is often put in a separate function, e.g. int max(x, y) that returns the greater of the two values.
    
    Input Format
    
    Input will contain four integers -  , one on each line.
    
    Output Format
    
    Print the greatest of the four integers.
    Note: I/O will be automatically handled.

---
+++ functionsInC.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int maxOfFour(int a, int b, int c, int d){
+    if (a>b && a>c && a>d) {
+        return a;
+    }
+    else if (b>a && b>c && b>d) {
+    return b;;
+    }
+    else if (c>a && c>b && c>d) {
+    return c;;
+    }
+    else {
+    return d;;
+    }
+    
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d\n %d\n %d\n %d", &a, &b, &c, &d);
+    int ans = maxOfFour(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
